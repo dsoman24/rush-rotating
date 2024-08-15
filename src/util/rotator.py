ba73dea5b9030036f0b29cec81e8354e69c559ee
danieloman1 <dsoman24@gmail.com>
2024-08-14 23:29:26 -0700

    add utils

---
+++ src/util/rotator.py
@@ -0,0 +1,34 @@
+"""This module contains Rotator, the driver for the rush sheet script."""
+
+import logging
+
+from lib.sheets import SheetEditor
+from pymongo import MongoClient
+
+logger = logging.getLogger(__name__)
+
+class Rotator:
+    """Driver class for the Rush rotating script."""
+
+    def __init__(
+            self,
+            client: MongoClient,
+            db_name: str,
+            spreadsheet_id: str
+        ):
+        """
+        Args:
+            client (MongoClient): The MongoDB client.
+            db_name (str): The name of the database to use.
+        """
+        self.client = client
+        self.db = client[db_name]
+        self.sheet_editor = SheetEditor(spreadsheet_id)
+
+    def execute(self):
+        logger.info("Updating the rotator data sheet.")
+        # Create the data sheet if it doesn't exist
+        self.sheet_editor.verify_or_create_data_sheet()
+        # Ensure the data sheet is empty
+        self.sheet_editor.clear_data_sheet()
+        logger.info("Rotator data sheet successfully updated.")
---
+++ src/util/sheets.py
@@ -0,0 +1,153 @@
+"""Module to interact with the Google Sheets API."""
+
+import os
+import logging
+
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+
+logger = logging.getLogger(__name__)
+
+_DATA_SHEET_NAME = 'data'
+
+class SheetEditor:
+    """Class to interact with one Google Sheets spreadsheet via Sheets API.
+
+    https://developers.google.com/sheets/api/guides/concepts?ref=hackernoon.com
+    """
+
+    def __init__(self, spreadsheet_id: str):
+        """Initialize the SheetEditor class.
+
+        Args:
+            spreadsheet_id (str): The ID of the Google Sheet.
+        """
+        self.spreadsheet_id = spreadsheet_id
+        credentials = self._get_credentials()
+        self.service = build("sheets", "v4", credentials=credentials)
+
+    def _get_credentials(self) -> service_account.Credentials:
+        """Gets the service account credentials.
+
+        Finds the first key file in the 'src/keys' directory if it exists.
+
+        Returns:
+            service_account.Credentials: The service account credentials.
+
+        Raises:
+            FileNotFoundError: If the 'keys' directory is not found or if no key
+                files are found.
+        """
+        if not os.path.exists("keys"):
+            error_msg = "'src/keys/' directory not found."
+            logger.error(error_msg)
+            raise FileNotFoundError(error_msg)
+        key_files = os.listdir("keys")
+        if not key_files:
+            error_msg = "No key files found in 'src/keys/' directory."
+            raise FileNotFoundError(error_msg)
+        # Choose first credential file in 'keys' directory
+        logger.info(f"Credentials created using key file: {key_files[0]}")
+        return service_account.Credentials.from_service_account_file(
+            f"keys/{key_files[0]}",
+            scopes=["https://www.googleapis.com/auth/spreadsheets"]
+        )
+
+    def _sheet_exists(self, sheet_name: str) -> bool:
+        """Checks if a sheet exists in the spreadsheet.
+
+        Args:
+            sheet_name (str): The name of the sheet to check.
+
+        Returns:
+            bool: True if the sheet exists, False otherwise.
+        """
+        logger.info(f"Checking if sheet '{sheet_name}' exists.")
+        sheet_metadata = self.service.spreadsheets().get(
+            spreadsheetId=self.spreadsheet_id
+        ).execute()
+        sheets = sheet_metadata.get("sheets", "")
+        for sheet in sheets:
+            if sheet["properties"]["title"] == sheet_name:
+                return True
+        return False
+
+    def _create_sheet(self, sheet_name: str) -> None:
+        """Creates a new sheet in the spreadsheet.
+
+        Args:
+            sheet_name (str): The name of the sheet to create.
+        """
+        logger.info(f"Creating sheet '{sheet_name}'.")
+        request = {
+            "requests": [
+                {
+                    "addSheet": {
+                        "properties": {
+                            "title": sheet_name
+                        }
+                    }
+                }
+            ]
+        }
+        self.service.spreadsheets().batchUpdate(
+            spreadsheetId=self.spreadsheet_id,
+            body=request
+        ).execute()
+
+    def _get_sheet_id(self, sheet_name: str) -> int:
+        """Gets the sheet ID using the sheet name.
+
+        Args:
+            sheet_name (str): The name of the sheet.
+
+        Returns:
+            int: The ID of the sheet.
+        """
+        sheets = self.service.spreadsheets().get(
+            spreadsheetId=self.spreadsheet_id
+        ).execute().get('sheets', [])
+        for sheet in sheets:
+            if sheet["properties"]["title"] == sheet_name:
+                return sheet["properties"]["sheetId"]
+        raise ValueError(f"Sheet with name {sheet_name} not found.")
+
+    def _clear_sheet(self, sheet_name: str) -> None:
+        """Clears the sheet of all data.
+
+        Args:
+            sheet_name (str): The name of the sheet to clear.
+        """
+        sheet_id = self._get_sheet_id(sheet_name)
+        request = {
+            "requests": [
+                {
+                    "updateCells": {
+                        "range": {
+                            "sheetId": sheet_id
+                        },
+                        "fields": "*"
+                    }
+                }
+            ]
+        }
+        self.service.spreadsheets().batchUpdate(
+            spreadsheetId=self.spreadsheet_id,
+            body=request
+        ).execute()
+
+    def verify_or_create_data_sheet(self):
+        """Verifies that the data sheet exists, creating it if necessary."""
+        if not self._sheet_exists(_DATA_SHEET_NAME):
+            logger.info(f"Creating {_DATA_SHEET_NAME} sheet.")
+            self._create_sheet(_DATA_SHEET_NAME)
+        else:
+            logger.info(f"{_DATA_SHEET_NAME} sheet already exists.")
+        if not self._sheet_exists(_DATA_SHEET_NAME):
+            error_msg = f"'{_DATA_SHEET_NAME}' sheet was not created."
+            logger.error(error_msg)
+            raise RuntimeError(error_msg)
+
+    def clear_data_sheet(self):
+        """Clears the data sheet of all data."""
+        self._clear_sheet(_DATA_SHEET_NAME)
