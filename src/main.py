51d31b4af25aac6e7207623ee900079b36915b52
danieloman1 <dsoman24@gmail.com>
2024-08-14 23:26:25 -0700

    first commit

---
+++ src/main.py
@@ -0,0 +1,32 @@
+"""Driver for the Rush rotating script."""
+
+import certifi
+import logging
+
+from dotenv import dotenv_values
+from pymongo import MongoClient
+from lib.rotator import Rotator
+
+logger = logging.getLogger(__name__)
+
+config = dotenv_values(".env")
+
+def main():
+    logging.basicConfig(
+        filename="main.log",
+        level=logging.INFO,
+        filemode="w",
+        format='%(asctime)s: %(name)s - %(levelname)s - %(message)s'
+    )
+    logger.info("Getting MongoDB Client.")
+    client = MongoClient(config["ATLAS_URI"], tlsCAFile=certifi.where())
+    rotator = Rotator(
+        client=client,
+        db_name=config["DB_NAME"],
+        spreadsheet_id=config["SHEET_ID"]
+    )
+    rotator.execute()
+    client.close()
+
+if __name__ == '__main__':
+    main()
